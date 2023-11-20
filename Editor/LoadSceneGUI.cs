9dfc4080f817184e6b432beba70be37f345bd79e
Anak Ladachat <70420610+Trissiklikk@users.noreply.github.com>
2023-11-21 03:54:59 +0700

    update patch note 1.1.0 - add favorite button.

--- Editor/LoadSceneGUI.cs
+++ Editor/LoadSceneGUI.cs
@@ -1,8 +1,13 @@
-#if UNITY_EDITOR
+﻿#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
+using Unity.Plastic.Newtonsoft.Json.Linq;
+using System.IO;
+using static Codice.CM.WorkspaceServer.WorkspaceTreeDataStore;
+using Unity.Plastic.Newtonsoft.Json;
+using System.Linq;
 
 namespace Trissiklikk.EditorTools
 {
@@ -17,6 +22,11 @@ namespace Trissiklikk.EditorTools
             GetWindow(typeof(LoadSceneGUI));
         }
 
+        private const string SAVE_FAVORITE_PATH = "Load_Scene_GUI";
+        private const string FILE_NAME = "FavoriteData.dat";
+
+        private static List<string> s_scenePathList;
+
         private bool m_isNeedSave;
         private int m_selectedTypeIndex;
         private string[] m_loadSceneTypes;
@@ -24,6 +34,7 @@ namespace Trissiklikk.EditorTools
 
         private LoadSceneGUI()
         {
+            s_scenePathList = new List<string>();
             m_scrollPosition = Vector2.zero;
             m_isNeedSave = true;
             m_selectedTypeIndex = 0;
@@ -33,6 +44,21 @@ namespace Trissiklikk.EditorTools
                 "Additive",
                 "Additive Without Loading",
             };
+
+            for (int i = 0; i < s_scenePathList.Count; i++)
+            {
+                Debug.Log(s_scenePathList[i]);
+            }
+        }
+
+        private void OnEnable()
+        {
+            LoadFavorite();
+        }
+
+        private void OnDestroy()
+        {
+            SaveFavotite();
         }
 
         private void OnGUI()
@@ -52,6 +78,11 @@ namespace Trissiklikk.EditorTools
             GUILayout.Space(10);
             EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
             List<EditorBuildSettingsScene> listScene = new List<EditorBuildSettingsScene>(scenes);
+            //listScene = new List<EditorBuildSettingsScene>(SortList(listScene));
+            for (int i = 0; i < s_scenePathList.Count; i++)
+            {
+                listScene = listScene.OrderByDescending(x => x.path.Contains("/" + s_scenePathList[i] + ".unity")).ToList();
+            }
             #region This is system to check if the path is in the list and group it.
             //listScene.Sort((x, y) => x.path.CompareTo(y.path));
             //List<string> lablePathName = new List<string>();
@@ -79,6 +110,15 @@ namespace Trissiklikk.EditorTools
                 GUIStyle styleBtnChangeScene = GetStyleButtonChangeScene(listScene[i].enabled);
                 GUIContent contentChangeScene = new GUIContent($"{sceneName}", $"Change Scene To {sceneName}");
                 EditorGUILayout.BeginHorizontal();
+                GUIStyle styleBtnFavorite = GetStyleFavoriteButton(sceneName);
+                GUIContent contentFavorite = new GUIContent("♥", "Favorite this scene");
+                if (GUILayout.Button(contentFavorite, styleBtnFavorite))
+                {
+                    if (!s_scenePathList.Contains(sceneName))
+                        s_scenePathList.Add(sceneName);
+                    else
+                        s_scenePathList.Remove(sceneName);
+                }
                 if (GUILayout.Button(contentChangeScene, styleBtnChangeScene))
                 {
                     if (m_isNeedSave)
@@ -111,6 +151,81 @@ namespace Trissiklikk.EditorTools
         }
 
         /// <summary>
+        /// Called when the window is opened.
+        /// </summary>
+        private void LoadFavorite()
+        {
+            string path = Path.Combine(Application.persistentDataPath, SAVE_FAVORITE_PATH);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                string content = File.ReadAllText(file);
+                JToken jsonToken = JsonConvert.DeserializeObject(content) as JToken;
+                CreateFavoriteFormJson(jsonToken);
+            }
+        }
+
+        /// <summary>
+        /// Called when the window is closed.
+        /// </summary>
+        private void SaveFavotite()
+        {
+            string path = Path.Combine(Application.persistentDataPath, SAVE_FAVORITE_PATH);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            string filePath = Path.Combine(path, FILE_NAME);
+            string json = FavoriteToJson().ToString();
+            File.WriteAllText(filePath, json);
+        }
+
+        /// <summary>
+        /// This method is called when the window is closed.
+        /// </summary>
+        /// <param name="json"></param>
+        private void CreateFavoriteFormJson(JToken json)
+        {
+            if (json is JArray)
+            {
+                JArray jArray = (JArray)json;
+                for (int i = 0; i < jArray.Count; i++)
+                {
+                    s_scenePathList.Add(jArray[i].Value<string>());
+                }
+            }
+        }
+
+        /// <summary>
+        /// This for save favorite data to json.
+        /// </summary>
+        /// <returns></returns>
+        private JToken FavoriteToJson()
+        {
+            JArray jArray = new JArray();
+            for (int i = 0; i < s_scenePathList.Count; i++)
+            {
+                jArray.Add(s_scenePathList[i]);
+            }
+            return jArray;
+        }
+
+        /// <summary>
+        /// This is stlye for button favorite.
+        /// </summary>
+        /// <returns></returns>
+        private GUIStyle GetStyleFavoriteButton(string sceneName)
+        {
+            GUIStyle styleBtnFavorite = new GUIStyle(GUI.skin.button);
+            styleBtnFavorite.fontSize = 12;
+            styleBtnFavorite.normal.textColor = CheckIsFavorite(sceneName) ? Color.red : Color.white;
+            styleBtnFavorite.hover.textColor = CheckIsFavorite(sceneName) ? Color.red : Color.white;
+            styleBtnFavorite.stretchHeight = false;
+            styleBtnFavorite.stretchWidth = false;
+            return styleBtnFavorite;
+        }
+
+        /// <summary>
         /// This is style for button load path.
         /// </summary>
         /// <returns></returns>
@@ -140,6 +255,21 @@ namespace Trissiklikk.EditorTools
             styleBtnChangeScene.normal.textColor = isEnable ? Color.white : Color.red;
             styleBtnChangeScene.hover.textColor = isEnable ? Color.white : Color.red;
             return styleBtnChangeScene;
+        }
+
+        /// <summary>
+        /// This function is used to check if the scene is in the favorite list.
+        /// </summary>
+        /// <param name="sceneName"></param>
+        /// <returns></returns>
+        private bool CheckIsFavorite(string sceneName)
+        {
+            for (int i = 0; i < s_scenePathList.Count; i++)
+            {
+                if (s_scenePathList[i] == sceneName)
+                    return true;
+            }
+            return false;
         }
 
         /// <summary>
